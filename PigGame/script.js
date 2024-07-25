df82b8a5e9337fe7a27920e5c3a8b13c5ae29fc9
Jayanth36 <mogulurijayanth@gmail.com>
2024-07-25 13:23:03 -0400

    Added Roll Dice Functionality

--- PigGame/script.js
+++ PigGame/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//Selecting Elements.
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const current0El = document.querySelector('#current--0');
+const diceRoll = document.querySelector('.btn--roll');
+
+let currentScore = 0;
+
+// starting conditions.
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//Rolling dice functionality
+diceRoll.addEventListener('click', function () {
+  // Generating random dice
+  let randomDice = Math.trunc(Math.random() * 6) + 1;
+
+  // Displaying dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${randomDice}.png`;
+
+  if (randomDice !== 1) {
+    currentScore += randomDice;
+    current0El.textContent = currentScore;
+  }
+});
