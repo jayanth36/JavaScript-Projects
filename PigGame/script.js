486cb5ce630824e4ea6448812377f7d40cbb1b9e
Jayanth36 <mogulurijayanth@gmail.com>
2024-07-30 18:25:38 -0400

    Added Hold Button FUnctionality,Restart button functionality and minor css changes

--- PigGame/script.js
+++ PigGame/script.js
@@ -1,30 +1,90 @@
 'use strict';
 
 //Selecting Elements.
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
 const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const diceRoll = document.querySelector('.btn--roll');
-
-let currentScore = 0;
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+let scores, currentScore, activePlayer, playing;
 
 // starting conditions.
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  currentScore = 0;
+  scores = [0, 0];
+  activePlayer = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+};
+
+init();
+// switching Player functionality
+const switchPlayer = function () {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling dice functionality
 diceRoll.addEventListener('click', function () {
-  // Generating random dice
-  let randomDice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    // Generating random dice
+    let randomDice = Math.trunc(Math.random() * 6) + 1;
 
-  // Displaying dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${randomDice}.png`;
-
-  if (randomDice !== 1) {
-    currentScore += randomDice;
-    current0El.textContent = currentScore;
+    // Displaying dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${randomDice}.png`;
+    //checking the dice value
+    if (randomDice !== 1) {
+      currentScore += randomDice;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
+});
+
+// Hold button functionality.
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //adding current score to player score
+    scores[activePlayer] += currentScore;
+    //Displaying player score
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // Winning logic
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  init();
 });
